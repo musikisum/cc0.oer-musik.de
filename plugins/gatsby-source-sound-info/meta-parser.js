41c09eebd309741acd81afbc2094c7b05eab5d11
Andreas Helmberger <public@ahelmberger.de>
2020-02-09 22:11:12 +0100

    Consolidation I

--- plugins/gatsby-source-sound-info/meta-parser.js
+++ plugins/gatsby-source-sound-info/meta-parser.js
@@ -150,13 +150,23 @@ module.exports = class MetaParser {
     }
 
     const trackKeys = fileObj.tracks.map(t => t.key);
+    if (fileObj.cdId === '002894793100') console.log('trackKeys', trackKeys);
     const metaKeys = Object.keys(metaObj).filter(m => !trackKeys.includes(m));
+    if (fileObj.cdId === '002894793100') console.log('metaKeys', metaKeys);
     const remainingMetas = Object.fromEntries(metaKeys.map(m => [m, metaObj[m]]));
+    if (fileObj.cdId === '002894793100') console.log('remainingMetas', remainingMetas);
+
+    const fileMetaKeys = Object.keys(metaObj).filter(m => trackKeys.includes(m));
+    if (fileObj.cdId === '002894793100') console.log('fileMetaKeys', fileMetaKeys);
+    const remainingFileMetas = Object.fromEntries(fileMetaKeys.map(m => [m, metaObj[m]]));
+    if (fileObj.cdId === '002894793100') console.log('remainingFileMetas', remainingFileMetas);
+
+    const finalMeta = Object.fromEntries(Object.entries(remainingMetas).filter(([k, v]) => finalFieldNames.includes(k)))
 
     return {
       id: `${fileObj.cdId}-${fileObj.tracks[0].key}`,
-      meta: { cdId: fileObj.cdId, ...remainingMetas },
-      tracks: fileObj.tracks
+      meta: { cdId: fileObj.cdId, ...finalMeta },
+      tracks: fileObj.tracks.map(t => ({ key: t.key, name: remainingFileMetas[t.key], fileName: t.fileName }))
     }
   }
 
