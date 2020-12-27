cd57d47af39d05451ce77fadbbbce303ca469b63
musikisum <ulrich.kaiser@hmtm.de>
2020-12-27 01:46:32 +0100

    Implement sorting

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,8 @@ export default () => {
     	<div className="jumbotron">
         <h1>Public Domain Musik</h1>
         <h2>Auf dieser Seite finden Sie Aufnahmen klassischer Musik, die nach deutschem Urheberecht nicht mehr geschützt sind.</h2>
-        <p>Die im Aufbau befindliche Sammlung besteht aus Digitalisaten von Schallplatten und CD's, auf denen Aufnahmen zu hören sind, die erstmalig vor dem 1. Januar 1963 aufgenommen und veröffentlicht worden sind. Weitere Informationen finden Sie auf den Seiten zum Urheberrecht und zum Leistungsschutzrecht.</p>
         <a className="btn btn-ob" href="/recordings">Zu den Aufnahmen...</a>
-      </div>      
+      </div>
     </Layout>
   )
 }
--- src/templates/recordings.js
+++ src/templates/recordings.js
@@ -4,14 +4,19 @@ import Layout from '../components/layout';
 import Search from '../components/search';
 
 export default ({ pageContext: { recordings } }) => {
-  
+  const compare = (a, b) => {
+    if ( a.meta.display < b.meta.display ) { return -1; }
+    if ( a.meta.display > b.meta.display ) { return 1; }
+    return 0;
+  }
+  recordings = recordings.sort(compare);
   return (
     <Layout>
       <div className="jumbotron">
         <h1>Public Domain Musik</h1>
         <Search recordings={recordings} />
         <h3>Aktuell sind {recordings.length} Aufnahmen verfügbar!</h3>
-      </div>      
+      </div>
       <div className="colums">
         {recordings.map(recording => (
           <div key={recording.id} className="recordingIsVisible">
