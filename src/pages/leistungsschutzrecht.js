be2146c549b0289e3d3758c9ba4f1b9e3d134d2b
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2020-12-30 02:16:35 +0100

    Create page sites

---
+++ src/pages/leistungsschutzrecht.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+
+export default ({ data }) => (
+	
+  <Layout>
+    <h1>Zum Leistungsschutzrecht (in Deutschland und der EU)</h1>
+    <p>Hier erscheint demnächst ein Text...</p>
+  </Layout>
+)
