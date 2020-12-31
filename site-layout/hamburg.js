350dd34cdab93be9b1467e2db09fb23fb187200e
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2020-12-31 01:12:17 +0100

    Delete old layout files

--- site-layout/hamburg.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".hamburg").onclick = function () {
-  this.classList.toggle("checked");
-  document.querySelector("#menuContainer").classList.toggle("mobileCollapsed");
-}
-
-
